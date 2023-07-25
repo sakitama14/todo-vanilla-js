5db6c3c223742631b5f5f4287a080479ab994ebc
sakitama14 <104491362+sakitama14@users.noreply.github.com>
2023-07-26 08:59:41 +0900

    タスクの追加機能（テキスト）

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import "./styles.css";
 
 const onClickbtn = () => {
-  alert();
+  // テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("input-text").value;
+  document.getElementById("input-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //devタグの子要素に各要素を設定
+  div.appendChild(li);
+  console.log(div);
+
+  //未完了リストに入力したものを追加する
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
