a23cd76bc09c3eb778d442a9b03bebb05ec6044d
sakitama14 <104491362+sakitama14@users.noreply.github.com>
2023-07-27 08:46:15 +0900

    タスクの機能追加（ボタン）

--- src/index.js
+++ src/index.js
@@ -13,9 +13,24 @@ const onClickbtn = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // 追加ボタンを作成する
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // 削除ボタンを作成する
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   //devタグの子要素に各要素を設定
   div.appendChild(li);
-  console.log(div);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに入力したものを追加する
   document.getElementById("incomplete-list").appendChild(div);
